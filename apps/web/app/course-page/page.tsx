3ed61634aea10dae4d66aba1da1a1dd8a5eb6c44
g33ya <gscozzaro2004@gmail.com>
2025-09-09 15:03:42 -0400

    Working link to course-specific page from homepage

--- apps/web/app/course-page/page.tsx
+++ apps/web/app/course-page/page.tsx
@@ -1,17 +1,16 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import styles from "../page.module.css";
 import Link from 'next/link';
+
 
 export default function Home() {
     return (
         <div className={styles.main}>
-                <h1>Courses</h1>
+                <h1>CISC474 Course Page</h1>
+                <br />
                 <hr />
 
-                 <Link href="/course-page">
-                    <button className={styles.button}>CISC474</button>
-                </Link>
         </div>
     );
 }
