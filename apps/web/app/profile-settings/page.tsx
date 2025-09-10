acb2cc20c978c50d713dc3f521ab1c87932cbf3f
g33ya <gscozzaro2004@gmail.com>
2025-09-10 15:08:10 -0400

    Basic "Profile Settings" page content/stylization

--- apps/web/app/profile-settings/page.tsx
+++ apps/web/app/profile-settings/page.tsx
@@ -1,14 +1,43 @@
-import Image, { type ImageProps } from "next/image";
+"use client";
+import Image from "next/image";
 import { Button } from "@repo/ui/button";
-import styles from "../page.module.css";
-import Link from 'next/link';
-
+import styles from "./page.module.css";
 
 export default function Home() {
-    return (
-        <div className={styles.main}>
-            <h1>Profile Settings</h1>
-            <hr />
+  return (
+    <div className={styles.main}>
+        <div className={styles.profileSettingsHeader}>
+          <h1>Profile Settings</h1>
         </div>
-    );
+
+        <hr />
+
+        <div className={styles.profileSettingsContent}>
+
+          <div className={styles.profileSettingsImage}>
+            <div className={styles.profileSettingsCircle}>
+              <Image
+                src="/profile-placeholder.jpg" 
+                alt="Profile"
+                width={200}
+                height={200}
+                className={styles.profileSettingsImg}
+              />
+            </div>
+          </div>
+
+          <div className={styles.profileSettingsInfo}>
+
+            <p><strong>Name:</strong> Jane Doe</p>
+            <p><strong>Pronouns:</strong> She/hers</p>
+            <p><strong>Roles:</strong> Learner</p>
+            <Button className={styles.profileSettingsEditButton}>Edit</Button>
+
+          </div>
+          
+        </div>
+    </div>
+  );
 }
+
+    
