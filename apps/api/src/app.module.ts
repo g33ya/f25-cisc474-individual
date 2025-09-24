65647486e586cb9a65c1ca550b80431176dd4ff7
g33ya <gscozzaro2004@gmail.com>
2025-09-24 17:01:19 -0400

    Enrollment module/controller/service created

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -8,9 +8,10 @@ import { UsersModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AssignmentModule } from './assignment/assignment.module';
 import { CourseModule } from './course/course.module';
+import { EnrollmentModule } from './enrollment/enrollment.module';
 
 @Module({
-  imports: [LinksModule, UsersModule, PrismaModule, AssignmentModule, CourseModule],
+  imports: [LinksModule, UsersModule, PrismaModule, AssignmentModule, CourseModule, EnrollmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
