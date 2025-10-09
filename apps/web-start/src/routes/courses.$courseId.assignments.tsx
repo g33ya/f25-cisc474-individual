a236301dcc756fe6c46d82fc83e32af8fdef2bab
g33ya <gscozzaro2004@gmail.com>
2025-10-09 12:56:21 -0400

    Basic course assignments page port

--- apps/web-start/src/routes/courses.$courseId.assignments.tsx
+++ apps/web-start/src/routes/courses.$courseId.assignments.tsx
@@ -1,40 +1,18 @@
-import styles from "./page.module.css";
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useParams } from "@tanstack/react-router"
+import styles from "../styles/course.module.css"
 
-export const Route = createFileRoute('/courses/$courseId/assignments')({
+export const Route = createFileRoute("/courses/$courseId/assignments")({
   component: AssignmentsPage,
 })
 
-// Hardcoded for now, will need to make dynamic when we have backend connected
-const courseAssignments: Record<string, { title: string; description: string }> = {
-  cisc474: {
-    title: "CISC474: Assignments",
-    description: "This is where CISC474 assignments will go!",
-  },
-  cisc361: {
-    title: "CISC361: Assignments",
-    description: "This is where CISC361 assignments will go!",
-  },
-};
-
-export default async function AssignmentsPage({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) {
-
-  const { slug } = await params;
-  const course = courseAssignments[slug];
-
-  if (!course) {
-    return <h2>Course assignments not found</h2>;
-  }
+function AssignmentsPage() {
+  const { courseId } = useParams({ from: "/courses/$courseId/assignments" })
 
   return (
     <div className={styles.main}>
-      <h2 className={styles.header}>{course.title}</h2>
+      <h2 className={styles.header}>{courseId.toUpperCase()} Assignments</h2>
       <hr />
-      <p>{course.description}</p>
+      <p>This is where assignments will go.</p>
     </div>
-  );
+  )
 }
